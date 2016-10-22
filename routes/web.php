20bedad69aedbff3ec8e748663c219a70caa96f8
Chukwuemeka Nwobodo <jc.nwobodo@gmail.com>
2016-10-22 13:15:10 +0100

    Update application routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,35 @@
 <?php
+/**
+ * Web Routes
+ *--------------------------------------------------------------------------
+**/
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
+if (config('app.env') == 'local' or config('app.debug')) {
+    $view_dir = '_tools.';
+    Route::group(['as' => '_tools.'], function () use ($view_dir) {
+        Route::get('/routes', ['as' => 'routes', 'uses' => 'WebController@showAppRoutes']);
+        Route::get('/console', ['as' => 'console', 'uses' => 'WebController@showAppConsole']);
+    });
+}
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['as' => 'app.'], function () {
+    Route::get('/', ['as' => 'home', 'uses' => 'WebController@showAppHome']);
+});
+
+//-------------AUTHENTICATION, REGISTRATION & PASSWORD RESET ROUES-----------------//
+Route::group(['as' => 'auth.', 'namespace' => 'Core\Auth'], function () {
+    // Authentication Routes...
+    Route::get('login', ['as' => 'login', 'uses' => 'LoginController@showLoginForm']);
+    Route::post('login', ['as' => 'login', 'uses' => 'LoginController@login']);
+    Route::post('logout', ['as' => 'logout', 'uses' => 'LoginController@logout'])->middleware(['m' => 'auth']);
+
+    // Registration Routes...
+    Route::get('signup', ['as' => 'signup', 'uses' => 'RegisterController@showRegistrationForm']);
+    Route::post('signup', ['as' => 'signup', 'uses' => 'RegisterController@register']);
+
+    // Password Reset Routes...
+    Route::get('password/reset', ['as' => 'password.reset', 'uses' => 'ForgotPasswordController@showLinkRequestForm']);
+    Route::post('password/email', ['as' => 'password.email', 'uses' => 'ForgotPasswordController@sendResetLinkEmail']);
+    Route::get('password/reset/{token}', ['as' => 'password.reset', 'uses' => 'ResetPasswordController@showResetForm']);
+    Route::post('password/reset', ['as' => 'password.reset', 'uses' => 'ResetPasswordController@reset']);
 });
